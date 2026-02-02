23a55d191fd94f7a4e47444ca4e8f701fb7e4beb
Anastasiya Mazur <98967429+idgafd@users.noreply.github.com>
2026-02-02 21:27:35 +0200

    change train/val/test split

--- data/dataframes.py
+++ data/dataframes.py
@@ -27,10 +27,10 @@ ratings = pd.read_csv(f'{_DATA_DIR}/ratings.dat',
                       encoding='latin-1')
 ratings['Timestamp'] = pd.to_datetime(ratings['Timestamp'], unit='s')
 
-# Temporal train / val / test split  (80 / 10 / 10 by timestamp)
+# Temporal train / val / test split  (75 / 12.5 / 12.5 by timestamp)
 
-_t1 = ratings['Timestamp'].quantile(0.8)
-_t2 = ratings['Timestamp'].quantile(0.9)
+_t1 = ratings["Timestamp"].quantile(0.75)
+_t2 = ratings["Timestamp"].quantile(0.875)
 
 train = ratings[ratings['Timestamp'] < _t1].copy()
 val = ratings[(ratings['Timestamp'] >= _t1) & (ratings['Timestamp'] < _t2)].copy()
